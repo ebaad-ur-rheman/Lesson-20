81fdc1b4525a05e668a3208cbbac7da859776149
ebaad-ur-rheman <ebaadur2@gmail.com>
2026-06-08 01:14:32 -0700

    Add files via upload

---
+++ Number_of_bits.py
@@ -0,0 +1,8 @@
+def NumberOfBits(n):
+    count = 0
+    while (n):
+        count +=1
+        n >>=1
+    return count
+number = int(input("Enter your number :"))
+print("Total bits : ",NumberOfBits(number))    
